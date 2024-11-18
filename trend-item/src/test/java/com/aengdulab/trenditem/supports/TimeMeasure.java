787bef9206bc687a14fcf3c29bbfa4d0aa056de6
juha <khabh@naver.com>
2024-11-18 12:07:24 +0900

    test: 테스트 로깅 수정

--- trend-item/src/test/java/com/aengdulab/trenditem/supports/TimeMeasure.java
+++ trend-item/src/test/java/com/aengdulab/trenditem/supports/TimeMeasure.java
@@ -1,12 +1,10 @@
 package com.aengdulab.trenditem.supports;
 
 import java.util.function.Supplier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TimeMeasure {
-
-    private static final Logger log = LoggerFactory.getLogger(TimeMeasure.class);
 
     public static <T> T measureTime(Supplier<T> supplier) {
         long startTime = System.currentTimeMillis();
