43633d94c53247bb762abe0faf42565ac5b616dd
Donghoon Lee <aru0504@naver.com>
2024-12-05 14:50:33 +0900

    refactor: 로직 및 테스트 리팩터링 (#23)
    * refactor: wrapper class 사용하지 않도록 수정
    
    * refactor: 테스트에서 `0` 매직넘버 사용하지 않고 메서드를 활용
    
    * refactor: 티켓 수량 검증을 도메인 책임으로 이동
    
    * refactor: 인당 티켓 수량 제한을 도메인 계층에서 서비스 계층으로 이동

--- ticket/src/main/java/com/aengdulab/ticket/service/MemberTicketService.java
+++ ticket/src/main/java/com/aengdulab/ticket/service/MemberTicketService.java
@@ -15,35 +15,34 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberTicketService {
 
+    private static final int MEMBER_TICKET_MAX_COUNT = 2;
+
     private final MemberRepository memberRepository;
     private final TicketRepository ticketRepository;
     private final MemberTicketRepository memberTicketRepository;
 
     @Transactional
-    public void issue(Long memberId, Long ticketId) {
+    public void issue(long memberId, long ticketId) {
         Member member = getMember(memberId);
         Ticket ticket = getTicket(ticketId);
-        validateIssuable(member, ticket);
+        validateIssuable(member);
         memberTicketRepository.save(new MemberTicket(member, ticket));
         ticket.decrementQuantity();
     }
 
-    private Member getMember(Long memberId) {
+    private Member getMember(long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException("멤버가 존재하지 않습니다."));
     }
 
-    private Ticket getTicket(Long ticketId) {
+    private Ticket getTicket(long ticketId) {
         return ticketRepository.findById(ticketId)
                 .orElseThrow(() -> new IllegalArgumentException("티켓이 존재하지 않습니다."));
     }
 
-    private void validateIssuable(Member member, Ticket ticket) {
-        if (!ticket.issuable()) {
-            throw new IllegalArgumentException("티켓 재고가 소진되었습니다.");
-        }
+    private void validateIssuable(Member member) {
         int issuedMemberTicketCount = memberTicketRepository.countByMember(member);
-        if (issuedMemberTicketCount >= MemberTicket.MEMBER_TICKET_COUNT_MAX) {
+        if (issuedMemberTicketCount >= MEMBER_TICKET_MAX_COUNT) {
             throw new IllegalArgumentException("계정당 구매할 수 있는 티켓 수량을 넘었습니다.");
         }
     }
